de73c98523462da659b51525ae0cbb36fc8f6443
Artsiom85 <artsiom.karaliou85@gmail.com>
2017-03-14 00:23:05 -0600

    Finished task in video 2.8 for Contact

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -9,19 +9,8 @@ public class ContactDeletionTests extends TestBase {
     public void ContactDeletionTests() {
         selectContact();
         deleteSelectedContacts();
-        app.getGroupHelper().wd.switchTo().alert().accept();
+        switchToWindowPopup();
         returnToHomePage();
     }
 
-    private void returnToHomePage() {
-        app.getGroupHelper().wd.findElement(By.linkText("home")).click();
-    }
-
-    private void deleteSelectedContacts() {
-        app.getGroupHelper().wd.findElement(By.xpath("//div[@id='content']/form[2]/div[2]/input")).click();
-    }
-
-    private void selectContact() {
-      app.getGroupHelper().selectGroup();
-    }
 }
--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/TestBase.java
@@ -1,6 +1,8 @@
 package by.stqa.pft.addressbook.tests;
 
 import by.stqa.pft.addressbook.appmanager.ApplicationManager;
+import by.stqa.pft.addressbook.model.ContactData;
+import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -23,5 +25,47 @@ public class TestBase {
 
   public ApplicationManager getApp() {
     return app;
+  }
+
+  protected void switchToWindowPopup() {
+    wd.switchTo().alert().accept();
+  }
+
+  protected void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(contactData.getHomephone());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  protected void initNewContactCreation() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  protected void returnToHomePage() {
+      wd.findElement(By.linkText("home page")).click();
+  }
+
+  protected void deleteSelectedContacts() {
+      wd.findElement(By.xpath("//div[@id='content']/form[2]/div[2]/input")).click();
+  }
+
+  protected void selectContact() {
+      wd.findElement(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img");
   }
 }
