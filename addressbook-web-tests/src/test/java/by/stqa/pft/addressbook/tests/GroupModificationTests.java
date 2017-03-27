01b483dc8e20cb20e439e57ea3d3dccef8077af4
Artsiom85 <artsiom.karaliou85@gmail.com>
2017-03-27 13:15:49 -0600

    Sets: Unordered collections for modifying groups

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import by.stqa.pft.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -20,11 +21,16 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test10", "test20", "test30");
     app.getGroupHelper().fillGroupForm(new GroupData("test10", "test20", "test30"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
