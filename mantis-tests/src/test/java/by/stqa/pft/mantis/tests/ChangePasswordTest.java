a175284c19d04ee59e1a8fd012ab15a939b03e85
Artsiom85 <artsiom.karaliou85@gmail.com>
2017-04-17 17:19:38 -0600

    20 Task. Test Implemented to change the password in Mantis

--- mantis-tests/src/test/java/by/stqa/pft/mantis/tests/ChangePasswordTest.java
+++ mantis-tests/src/test/java/by/stqa/pft/mantis/tests/ChangePasswordTest.java
@@ -43,13 +43,6 @@ public class ChangePasswordTest extends TestBase{
     assertTrue(app.newSession().login(modifiedUser.getName(), password));
   }
 
-  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
-    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
-    VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
-    return regex.getText(mailMessage.text);
-  }
-
-
 
   @AfterMethod(alwaysRun = true)
   public void stopMailServer(){
