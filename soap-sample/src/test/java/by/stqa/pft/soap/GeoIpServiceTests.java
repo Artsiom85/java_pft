558f57419d8acb37f58224bc69a5aa0f3e28e996
Artsiom85 <artsiom.karaliou85@gmail.com>
2017-04-19 00:32:49 -0600

    9.2 Lection

---
+++ soap-sample/src/test/java/by/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,20 @@
+package by.stqa.pft.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by Artsiom on 4/18/2017.
+ */
+public class GeoIpServiceTests {
+
+
+  @Test
+  public void testMyIp() {
+    GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("50.235.209.170");
+    assertEquals(geoIP.getCountryCode(), "UKR");
+  }
+}
